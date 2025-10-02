71ff5216908147e203f26b65bb7a118120a92fb3
Raymond <linghenghua719@gmail.com>
2025-10-02 20:33:46 +0800

    Up

--- Assets/_AssetsRaymond/Scripts/Managers/MainMenuManager.cs
+++ Assets/_AssetsRaymond/Scripts/Managers/MainMenuManager.cs
@@ -19,4 +19,22 @@ public class MainMenuManager : MonoBehaviour
         }
         SceneManager.LoadScene("LobbyScene"); // Replace with your exact scene name
     }
+
+    // Called when the Exit button is pressed
+    public void ExitButtonPressed()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlaySFX("Button Pressed");
+        }
+        
+        // Quit the application
+        #if UNITY_EDITOR
+            // If running in the Unity Editor, stop playing
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            // If running as a built application, quit the application
+            Application.Quit();
+        #endif
+    }
 }
